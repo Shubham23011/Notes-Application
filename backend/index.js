4aa59fd497cb3fef7aef736ef746cfe1504f73d3
Shubham <shubhamraihero2015@gmail.com>
2024-10-14 22:36:49 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -13,23 +13,29 @@ const PORT = process.env.PORT || 8000; // Use environment variable for port
 
 app.use(express.json());
 
-// Define allowed origins
-const allowedOrigins = ['http://localhost:5173', 'https://notes-application-0xpz.onrender.com','https://notes-application-2.onrender.com'];
+// // Define allowed origins
+// const allowedOrigins = ['http://localhost:5173', 'https://notes-application-0xpz.onrender.com','https://notes-application-2.onrender.com'];
 
-// CORS configuration
+// // CORS configuration
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+// app.options('*', cors()); // Enable preflight requests for all routes
+
 app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+        origin: "https://slug-panel.onrender.com"
     }
-    return callback(null, true);
-  }
-}));
-
-app.options('*', cors()); // Enable preflight requests for all routes
+))
+app.options('*', cors())
 
 // Connect to MongoDB with error handling
 mongoose.connect(config.connectionString)
