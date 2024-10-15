c0b6563e9db231a6ef788eb1c164ffdfa8be6c4f
Shubham <shubhamraihero2015@gmail.com>
2024-10-15 14:36:42 +0530

    complete

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,8 @@ const allowedOrigins = [
   'https://rainbow-kelpie-1ec452.netlify.app',
   'https://legendary-capybara-a0c644.netlify.app/login',
   'http://localhost:5173',
-  'https://notes-application-orcin.vercel.app/login'
+  'https://notes-application-orcin.vercel.app/login',
+  'https://notes-manager-frontend.onrender.com/login'
 ];
 
 // Apply CORS middleware
